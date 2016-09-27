0d14bc779c7471c64698df2e99503ac22d10fb28
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2016-09-27 12:13:55 -0400

    finish some of the plotting for the eye sensitivity stuff

--- pset3/transformCoords.m
+++ pset3/transformCoords.m
@@ -10,9 +10,9 @@ wavelength = [360 : 5 : 730];
 % want to solve for t such that C t = xi, with xi being the basis vectors (1,0,0), (0,1,0), (0,0,1).
 % In addition we need t \geq 0 since this is supposed to be a spectrum.
 
-p_r_CIE = lsqlin(ones(1,N), 0, [],[],C_RGB,[1;0;0],zeros(N,1),[]);
-p_r_CIE = lsqlin(ones(1,N), 0, [],[],C_RGB,[0;1;0],zeros(N,1),[]);
-p_r_CIE = lsqlin(ones(1,N), 0, [],[],C_RGB,[0;0;1],zeros(N,1),[]);
+p_r_RGB = lsqlin(ones(1,N), 0, [],[],C_RGB,[1;0;0],zeros(N,1),[]);
+p_g_RGB = lsqlin(ones(1,N), 0, [],[],C_RGB,[0;1;0],zeros(N,1),[]);
+p_b_RGB = lsqlin(ones(1,N), 0, [],[],C_RGB,[0;0;1],zeros(N,1),[]);
 
 
 
@@ -20,9 +20,9 @@ p_r_CIE = lsqlin(ones(1,N), 0, [],[],C_RGB,[0;0;1],zeros(N,1),[]);
 fig = figure(1);
 clf(fig);
 hold on;
-plot(wavelength, p_r, 'r');
-plot(wavelength, p_g, 'g');
-plot(wavelength, p_b, 'b');
+plot(wavelength, p_r_RGB, 'r');
+plot(wavelength, p_g_RGB, 'g');
+plot(wavelength, p_b_RGB, 'b');
 title('spectra of primary colors for RGB color system')
 xlabel('wavelength');
 ylabel('power');
@@ -31,8 +31,8 @@ hold off;
 
 % primary colors for CIE color system
 p_r_CIE = lsqlin(ones(1,N), 0, [],[],C,[1;0;0],[],[]);
-p_r_CIE = lsqlin(ones(1,N), 0, [],[],C,[0;1;0],[],[]);
-p_r_CIE = lsqlin(ones(1,N), 0, [],[],C,[0;0;1],[],[]);
+p_g_CIE = lsqlin(ones(1,N), 0, [],[],C,[0;1;0],[],[]);
+p_b_CIE = lsqlin(ones(1,N), 0, [],[],C,[0;0;1],[],[]);
 
 
 % plot the power spectra as a function of wavelength
@@ -62,7 +62,31 @@ hold off;
 
 % load the eye's spectral response curves
 load('LMSResponse.mat')
+goodIdx = 7:75;
+lmsShort = LMSResponse(:,goodIdx);
+N_short = length(goodIdx);
 
-L = C_RGB*LMSResponse(1,:)';
-M = C_RGB*LMSResponse(2,:)';
-S = C_RGB*LMSResponse(3,:)';
+p_L_eye = lsqlin(ones(1,N_short), 0, [],[],lmsShort,[1;0;0],[],[]);
+p_M_eye = lsqlin(ones(1,N_short), 0, [],[],lmsShort,[0;1;0],[],[]);
+p_S_eye = lsqlin(ones(1,N_short), 0, [],[],lmsShort,[0;0;1],[],[]);
+
+
+p_L_eye = pinv(lmsShort)*[1;0;0];
+p_M_eye = pinv(lmsShort)*[0;1;0];
+p_S_eye = pinv(lmsShort)*[0;0;1];
+
+wavelengthShort = wavelength(goodIdx);
+
+
+%% Plot color matching functions for RGB system
+fig = figure(4);
+clf(fig);
+hold on;
+plot(wavelengthShort, p_L_eye, 'r', 'DisplayName', 'L')
+plot(wavelengthShort, p_M_eye, 'g', 'DisplayName', 'M')
+plot(wavelengthShort, p_S_eye, 'b', 'DisplayName', 'S')
+title('Primary color spectra for eye spectral sensitivity curves')
+xlabel('wavelength')
+legend('show')
+hold off;
+
